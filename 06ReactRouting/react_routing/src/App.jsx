2373688ad0d247bcd61bd8776e2bec2cf5480b77
{Harshit-Learn} <harshitbaranwal609@gmail.com>
2025-08-19 00:15:58 +0530

    React Routing

---
+++ 06ReactRouting/react_routing/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from "./components/Navbar/Navbar.jsx"
+
+function App() {
+   
+return (
+  <div className="app">
+  <Navbar/>
+
+   <main className="app_main">{/*Perform Routing Here}</main> */}</main>
+  </div>
+  );
+};
+
+export default App
---
+++ 06ReactRouting/react_routing/src/components/Articles/Articles.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Articles = () => {
+  return (
+    <div>
+      <h2>Articles
+      <p>SortBy: Categary:</p>
+      </h2>
+    </div>
+  )
+}
+
+export default Articles
